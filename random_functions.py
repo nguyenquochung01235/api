fb147a9ffa78d44d10c1b0a2d50e9ee28e1ec72f
nguyenquohung01235 <nguyenquochung01235@gmail.com>
2022-07-13 14:28:38 +0700

    update feature text field 2

--- random_functions.py
+++ random_functions.py
@@ -527,7 +527,7 @@ class api():
 		if args[0] != "":
 			return args[0]
 		else:
-			return "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+			return "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
 
 	###API for random id ###
 	def random_id(self):
